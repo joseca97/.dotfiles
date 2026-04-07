861eb3196b18732c5aa5377f938b22474a3d11a6
Jose Carlos Arinero <arinero97@gmail.com>
2026-04-08 01:10:07 +0200

    Update nvim and hypr

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -104,6 +104,7 @@ vim.diagnostic.config {
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 vim.keymap.set('n', '<leader>d', '<cmd>:DocsViewToggle<CR>')
+vim.keymap.set('n', '<leader>k', '<cmd>:WhichKey<CR>')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
